574b9d974b52aa005c47ef1386b10ffa817982a4
jefferson <jefferson.nascimento@fastsolucoes.com.br>
2021-11-15 17:07:19 -0300

    Atualização mult, fazendo-o funcionar com o docker

---
+++ microsservicos/mult2/app.js
@@ -0,0 +1,8 @@
+const express = require ("express");
+const app = express ();
+
+app.get("/mult2", function(req, res){
+    res.end("Resultado: " + req.query.op1 * req.query.op2);
+});
+
+app.listen(3000);
--- mult/app.js
+++
@@ -1,8 +0,0 @@
-const express = require ("express");
-const app = express ();
-
-app.get("/mult", function(req, res){
-    res.end("Resultado: " + req.query.op1 * req.query.op2);
-})
-
-app.listen(3000);
